468c25917d162faa6450237ae6e9df92fcc55e23
Hassan Alzaid <7soony2818@gmail.com>
2025-02-12 17:18:38 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <input v-model="todo" type="text">
+  <button @click="add">add</button>
+  <div v-for="task in todos">
+    <div>
+      {{ task.title }}
+    </div>
+  </div>
+</template>
+
+<script setup>
+import {ref} from 'vue'
+let todos=ref([''])
+let todo=ref('')
+function add() {
+  todos.value.push({
+    complate:false, title:todo.value
+  })
+  todo.value=''
+}
+</script>
